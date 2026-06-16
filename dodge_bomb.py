7436d864edccaf49242d7e8965a2731bd16c0c76
nakano <c0a25184cf@edu.teu.ac.jp>
2026-06-16 16:37:09 +0900

    演習課題１と２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
-
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -13,6 +13,47 @@ DELTA = {
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# 演習1
+def gameover(screen: pg.Surface) -> None:
+    """
+    ゲームオーバー画面を表示する関数
+    引数：描画先のscreen Surface
+    """
+    black_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(black_img, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
+    black_img.set_alpha(150) 
+    screen.blit(black_img, [0, 0])
+
+    font_o = pg.font.Font(None, 80)
+    txt = font_o.render("Game Over", True, (255, 255, 255))
+    screen.blit(txt, [WIDTH//2 - 150, HEIGHT//2])
+
+    kk_cry = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    screen.blit(kk_cry, [WIDTH//2 - 250, HEIGHT//2 - 20])
+    screen.blit(kk_cry, [WIDTH//2 + 200, HEIGHT//2 - 20])
+
+    pg.display.update()
+    time.sleep(5)
+
+# 演習２
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    時間経過で拡大する爆弾のリストと加速度のリストを生成する関数
+    戻り値：(爆弾Surfaceのリスト, 加速度のリスト)
+    """
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)] 
+    
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+        
+    return bb_imgs, bb_accs
+
+
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -42,6 +83,10 @@ def main():
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5 
 
+    # 演習課題２
+    bb_imgs, bb_accs = init_bb_imgs()
+    bb_rct = bb_imgs[0].get_rect() 
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -50,8 +95,20 @@ def main():
                 return
 
         if kk_rct.colliderect(bb_rct):  # こうかとんRectと爆弾Rectが重なったら
+            gameover(screen)
             print("ゲームオーバー")
             return
+        
+        # 演習課題２
+        idx = min(tmr // 500, 9)
+        current_bb_img = bb_imgs[idx]
+        avx = vx * bb_accs[idx]
+        avy = vy * bb_accs[idx]
+        bb_rct.width = current_bb_img.get_rect().width
+        bb_rct.height = current_bb_img.get_rect().height
+        bb_rct.move_ip(avx, avy) # 加速された速度で移動させる
+        screen.blit(current_bb_img, bb_rct) # 描画するときは現在のサイズの画像を使う
+        tmr += 1
         
         screen.blit(bg_img, [0, 0]) 
 
